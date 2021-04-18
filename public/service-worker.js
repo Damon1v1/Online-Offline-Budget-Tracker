f61726e6c1ee0e7edcb1d00795ca319bff8bea82
Damon Burda <damonburda96@gmail.com>
2021-04-18 14:42:15 -0700

    cache install steps

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,22 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "/",
-    "/index",
+    "/db.js",
+    "/manifest.json",
+    "/index.js",
     "/styles.css",
     "/dist/bundle.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-]
+];
+
+self.addEventListener("install", function(evt) {
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });

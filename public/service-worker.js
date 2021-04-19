78fea48380d05aeb4e513cb48a7a80ad2ea0738e
Damon Burda <damonburda96@gmail.com>
2021-04-18 18:43:58 -0700

    caching and seving requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -20,3 +20,42 @@ self.addEventListener("install", function(evt) {
       })
     );
   });
+
+  self.addEventListener("fetch", function(evt) {
+    // cache get requests /api 
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // clone and store in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      fetch(evt.request).catch(function() {
+        return caches.match(evt.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
+  
